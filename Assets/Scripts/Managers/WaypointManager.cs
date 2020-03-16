425efc0569f6ce65908e462c477e3e4f18aff0f7
Xenia Moros <xmoros@gmail.com>
2020-03-16 17:56:00 +0100

    Adapt To WaypointManager
    Adapt elevadores y camaras al WaypointManager

--- Assets/Scripts/Managers/WaypointManager.cs
+++ Assets/Scripts/Managers/WaypointManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WaypointManager : MonoBehaviour
 {
     public List<WaypointScript> waypointList;
-    int waypointActual = 0;
+    internal int waypointActual = 0;
     int operacionWaypoint = 1;
     public bool rebote;
 
@@ -35,6 +35,34 @@ public class WaypointManager : MonoBehaviour
         }
 
         
+    }
+
+    public void RetrocederWaypoint()
+    {
+        waypointActual -= operacionWaypoint;
+
+        if (rebote)
+        {
+            if (waypointActual <= 0)
+            {
+                operacionWaypoint = 1;
+                waypointActual = 0;
+            }
+            else if (waypointActual >= waypointList.Count - 1)
+            {
+                operacionWaypoint = -1;
+                waypointActual = waypointList.Count - 1;
+            }
+        }
+        else
+        {
+            if (waypointActual >= waypointList.Count)
+            {
+                waypointActual = 0;
+            }
+        }
+
+
     }
 
     public WaypointScript RetornarWaypoint()
