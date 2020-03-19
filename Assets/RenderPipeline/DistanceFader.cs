4702b2254eaea3e26fbf45ec786bdd5f0bdc3553
Xenia Moros <xmoros@gmail.com>
2020-03-19 19:22:16 +0100

    Shader Improvement

---
+++ Assets/RenderPipeline/DistanceFader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceFader : MonoBehaviour
+{
+    Material material;
+    Transform alpacaPosition;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        alpacaPosition = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        material.SetVector("_Alpaca_Pos", alpacaPosition.position);
+    }
+}
