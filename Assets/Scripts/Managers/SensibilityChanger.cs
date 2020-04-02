268e960dd7a3dd15543e974d2fa3cce770759cd1
Xenia Moros <xmoros@gmail.com>
2020-04-02 19:20:35 +0200

    Sensibility and invert axis work

---
+++ Assets/Scripts/Managers/SensibilityChanger.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SensibilityChanger : MonoBehaviour
+{
+    public Slider slider;
+
+    public void OnSliderChange()
+    {
+        StaticManager.ChangeSensibility(slider.value);
+    }
+}
--- Assets/Scripts/Managers/StaticManager.cs
+++ Assets/Scripts/Managers/StaticManager.cs
@@ -4,23 +4,27 @@ using UnityEditor;
 public static class StaticManager
 {
     [Range(-5,5)] public static sbyte brightness = 0;
-    [Range(0.2f, 10)] public static float sensibility = 1;
+    [Range(0.1f, 10)] public static float sensibility = 1;
     public static bool axisV = false, axisH = false;
     public static bool pause = true;
+    public static bool cameraOptionsChanged = false;
     
     public static void ChangeAxisV()
     {
         axisV = !axisV;
+        cameraOptionsChanged = true;
     }
 
     public static void ChangeAxisH()
     {
         axisH = !axisH;
+        cameraOptionsChanged = true;
     }
 
     public static void ChangeSensibility(float sens)
     {
         sensibility = sens;
+        cameraOptionsChanged = true;
     }
     public static void ChangeBrightness(int bright)
     {
