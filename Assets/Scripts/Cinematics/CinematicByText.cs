cf51b64c9de3ab295755972d44746bac498998a7
Xenia Moros <xmoros@gmail.com>
2020-06-10 22:17:26 +0200

    Cinematichs controller

---
+++ Assets/Scripts/Cinematics/CinematicByText.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions;
+
+public class CinematicByText : CinematicManager
+{
+
+    public TextAsset file;
+
+    internal override void Start()
+    {
+        base.Start();
+        
+    }
+    internal override void Update()
+    {
+        base.Update();
+    }
+
+    internal override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+
+    internal override void RellenarArrayAnimaciones()
+    {
+        string[] fLines = Regex.Split(file.text, "\n");
+
+        foreach (string value in fLines)
+        {
+            Debug.Log(value);
+        }
+
+        arrayAnimaciones = new AnimationGuide[fLines.Length];
+
+        for (int i = 0; i < fLines.Length; i++)
+        {
+            string valueLine = fLines[i];
+            string[] values = Regex.Split(valueLine, ";");
+
+            string name = values[0];
+            float time =  float.Parse(values[1]);
+            bool moving;
+
+            if (int.Parse(values[2]) == 0) moving = false;
+            else moving = true;
+
+            float delay = float.Parse(values[3]);
+            float finish = float.Parse(values[4]);
+
+            arrayAnimaciones[i] = new AnimationGuide(name, time, moving, delay, finish);
+        }
+
+        Debug.Log(arrayAnimaciones.Length);
+
+        foreach(AnimationGuide animationGuide in arrayAnimaciones)
+        {
+            Debug.Log(animationGuide.animationName);
+            Debug.Log(animationGuide.animationLength);
+            Debug.Log(animationGuide.animationIsMoving);
+        }
+    }
+}
--- Assets/Scripts/Cinematics/CinematicManager.cs
+++ Assets/Scripts/Cinematics/CinematicManager.cs
@@ -4,15 +4,128 @@ using UnityEngine;
 
 public class CinematicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    public WaypointManager waipoints;
+
+    float animationTimer;
+    float animationTime;
+    internal bool animacionAcabada;
+    internal int animNumber;
+    internal AnimationGuide[] arrayAnimaciones;
+    Vector3 direction;
+    float velocity;
+    float finishMovement;
+
+    RuntimeAnimatorController ac;
+    internal virtual void Start()
     {
-        
+        ac = animator.runtimeAnimatorController;
+        RellenarArrayAnimaciones();
+        animNumber = 0;
+        ReadAnimation(ref animNumber);
+        animator.SetTrigger(arrayAnimaciones[animNumber].animationName);
+        animationTimer = 0;
+        animacionAcabada = false;
     }
 
     // Update is called once per frame
-    void Update()
+    internal virtual void Update()
     {
-        
+        if (arrayAnimaciones[animNumber].animationIsMoving &&
+            animationTimer >= arrayAnimaciones[animNumber].animationDelayMovement &&
+            animationTimer <= finishMovement)
+        {
+            this.transform.Translate(direction * velocity * Time.deltaTime, Space.World);
+        }
+
+        animationTimer += Time.deltaTime;
+
+        if (animationTimer >= animationTime)
+        {
+            animacionAcabada = true;
+        }
+    }
+
+    internal virtual void LateUpdate()
+    {
+
+        if (animacionAcabada)
+        {
+            animationTimer = 0;
+            animNumber += 1;
+            ReadAnimation(ref animNumber);
+            animator.SetTrigger(arrayAnimaciones[animNumber].animationName);
+
+            animacionAcabada = false;
+        }
+    }
+
+    internal void ReadAnimation(ref int animationNumber)
+    {
+        if(animationNumber >= arrayAnimaciones.Length)
+        {
+            animationNumber = 0;
+        }
+
+        if(arrayAnimaciones[animationNumber].animationLength > 0)
+        {
+            animationTime = arrayAnimaciones[animationNumber].animationLength;
+        }
+        else
+        {
+            for (int i = 0; i < ac.animationClips.Length; i++)
+            {
+                if (ac.animationClips[i].name == arrayAnimaciones[animationNumber].animationName)
+                {
+                    animationTime = ac.animationClips[i].length;
+                }
+            }
+        }
+
+        if(arrayAnimaciones[animationNumber].animationIsMoving)
+        {
+            waipoints.AvanzarWaypoint();
+            direction = waipoints.RetornarWaypoint().RetornarPosition() - transform.position;
+            velocity = direction.magnitude / (animationTime - arrayAnimaciones[animationNumber].animationDelayMovement - arrayAnimaciones[animationNumber].animationFinishMovement);
+            direction.Normalize();
+
+            transform.forward = new Vector3(direction.x, 0, direction.z).normalized;
+            finishMovement = animationTime - arrayAnimaciones[animationNumber].animationFinishMovement;
+        }
+
+
+    }
+
+    internal virtual void RellenarArrayAnimaciones()
+    {
+        arrayAnimaciones = new AnimationGuide[1];
+        arrayAnimaciones[0] = new AnimationGuide("Idle", 0, false);
+    }
+}
+
+internal struct AnimationGuide
+{
+    public string animationName;
+    public float animationLength;
+    public bool animationIsMoving;
+    public float animationDelayMovement;
+    public float animationFinishMovement;
+
+    public AnimationGuide(string aName,float aLength,bool aIsMoving)
+    {
+        animationName = aName;
+        animationLength = aLength;
+        animationIsMoving = aIsMoving;
+        animationDelayMovement = 0;
+        animationFinishMovement = 0;
+    }
+
+    public AnimationGuide(string aName, float aLength, bool aIsMoving, float aDelayMovement, float aFinishMovement)
+    {
+        animationName = aName;
+        animationLength = aLength;
+        animationIsMoving = aIsMoving;
+        animationDelayMovement = aDelayMovement;
+        animationFinishMovement = aFinishMovement;
     }
 }
